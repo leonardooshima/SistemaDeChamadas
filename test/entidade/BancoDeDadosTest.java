74b89b94cea4cb72a61b9d7d31ae21e70fbe024e
leona <leona@Lo>
2019-04-29 22:23:30 -0300

    Testes das três primeiras classes do pacote entidade

--- test/entidade/BancoDeDadosTest.java
+++ test/entidade/BancoDeDadosTest.java
@@ -35,29 +35,5 @@ public class BancoDeDadosTest {
     
     @After
     public void tearDown() {
-    }
-
-    /**
-     * Test of values method, of class BancoDeDados.
-     */
-    @Test
-    public void testValues() {
-        System.out.println("values");
-        BancoDeDados[] expResult = null;
-        BancoDeDados[] result = BancoDeDados.values();
-        assertArrayEquals(expResult, result);
-    }
-
-    /**
-     * Test of valueOf method, of class BancoDeDados.
-     */
-    @Test
-    public void testValueOf() {
-        System.out.println("valueOf");
-        String name = "";
-        BancoDeDados expResult = null;
-        BancoDeDados result = BancoDeDados.valueOf(name);
-        assertEquals(expResult, result);
-    }
-    
+    }    
 }
